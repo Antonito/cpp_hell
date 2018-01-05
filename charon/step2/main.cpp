976bea17128cd601f4d51be3641d2862a2f972b8
Ludonope <ludovic.petrenko@epitech.eu>
2018-01-05 22:03:47 +0100

    Added support for 1 param

--- charon/step2/main.cpp
+++ charon/step2/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
 #include "Function.hpp"
 
 int zeroFunction()
@@ -8,11 +9,20 @@ int zeroFunction()
 	return 0;
 }
 
+int oneFunction(std::string const &str)
+{
+	std::cout << str << std::endl;
+	return 0;
+}
+
 int main()
 {
-	Function<int()> f0;
-	f0 = &zeroFunction;
+	Function<int()> const f0(&zeroFunction);
 	
 	f0();
+
+	Function<int(std::string const &)> f1(&oneFunction);
+
+	f1("Hello world!");
 	return EXIT_SUCCESS;
 }
