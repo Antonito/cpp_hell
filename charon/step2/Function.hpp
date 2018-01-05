c096a5814e10f1ef83ee286f26aa6d9484a7e182
Ludonope <ludovic.petrenko@epitech.eu>
2018-01-05 21:37:26 +0100

    Added task 2

--- charon/step2/Function.hpp
+++ charon/step2/Function.hpp
@@ -1,4 +1,74 @@
 #ifndef FUNCTION_HPP_
 #define FUNCTION_HPP_
 
+template <typename R, typename Arg1 = void, typename Arg2 = void, typename Arg3 = void, typename Arg4 = void>
+class Function;
+
+template <typename R>
+class Function<R()>
+{
+public:
+	class ICallable
+	{
+	public:
+		virtual ~ICallable() {}
+		virtual R Invoke() = 0;
+	};
+
+	template <typename T>
+	class Callable : public ICallable
+	{
+	public:
+		Callable(T t) : m_t(t) {}
+		virtual ~Callable() {}
+		R Invoke()
+		{
+			return m_t();
+		}
+	private:
+		T m_t;
+	};
+	
+	Function() : m_callable(NULL)
+	{
+	}
+
+	template <typename T>
+	Function(T t) : m_callable(new Callable(t))
+	{
+	}
+
+	template <typename T>
+	Function &operator=(T t)
+	{
+		if (!m_t)
+		{
+			throw std::exception();
+		}
+		return (*t)();
+	}
+private:
+	ICallable *m_callable;
+};
+//
+//template <typename R, typename Arg1>
+//class Function<R(Arg1)>
+//{
+//};
+//
+//template <typename R, typename Arg1, typename Arg2>
+//class Function<R(Arg1, Arg2)>
+//{
+//};
+//
+//template <typename R, typename Arg1, typename Arg2, typename Arg3>
+//class Function<R(Arg1, Arg2, Arg3)>
+//{
+//};
+//
+//template <typename R, typename Arg1, typename Arg2, typename Arg3, typename Arg4>
+//class Function<R(Arg1, Arg2, Arg3, Arg4)>
+//{
+//};
+
 #endif
--- charon/step2/main.cpp
+++ charon/step2/main.cpp
@@ -1,7 +1,18 @@
 #include <cstdlib>
+#include <iostream>
 #include "Function.hpp"
+
+int zeroFunction()
+{
+	std::cout << "Yeah" << std::endl;
+	return 0;
+}
 
 int main()
 {
+	Function<int()> f0;
+	f0 = &zeroFunction;
+	
+	f0();
 	return EXIT_SUCCESS;
 }
