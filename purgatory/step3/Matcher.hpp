50ddc28a1505ab79550edee78542c3930fbd54e7
Ludonope <ludovic.petrenko@epitech.eu>
2018-01-06 12:37:06 +0100

    Made the purgatory matcher v1.0

---
+++ purgatory/step3/Matcher.hpp
@@ -0,0 +1,18 @@
+#ifndef MATCHER_HPP_
+#define MATCHER_HPP_
+
+#include "FSA.hpp"
+
+class Matcher
+{
+public:
+	explicit Matcher(FSA &fsa);
+
+	bool find(std::string const &str);
+	bool find(std::string const &str, std::size_t &count);
+
+private:
+	FSA &m_fsa;
+};
+
+#endif // !MATCHER_HPP_
---
+++ purgatory/step3/State.hpp
@@ -0,0 +1,64 @@
+#ifndef STATE_HPP_
+#define STATE_HPP_
+
+#include <string>
+
+class State
+{
+public:
+	State();
+	State(std::string const &name);
+	State(State const &other);
+	~State();
+
+	State &operator=(State const &other);
+
+	bool operator==(State const &rhs) const;
+	bool operator!=(State const &rhs) const;
+	bool operator>(State const &rhs) const;
+	bool operator<(State const &rhs) const;
+	bool operator>=(State const &rhs) const;
+	bool operator<=(State const &rhs) const;
+
+	static State create();
+
+	void linkTo(State &s, char c);
+
+	std::string const &name() const
+	{
+		return m_name;
+	}
+
+	bool isFinal() const
+	{
+		return m_final;
+	}
+
+	void setFinal(bool isFinal)
+	{
+		m_final = isFinal;
+	}
+
+	State *getLink()
+	{
+		return m_nextState;
+	}
+
+	State const *getLink() const
+	{
+		return m_nextState;
+	}
+
+	char getLinkLetter() const
+	{
+		return m_charToNext;
+	}
+
+private:
+	std::string m_name;
+	bool m_final;
+	State *m_nextState;
+	char m_charToNext;
+};
+
+#endif // !STATE_HPP_
