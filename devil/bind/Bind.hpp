b3df3c3b51e22e11f021e271200173b5b053d5d8
Antoine Bache <antoine.bache@epitech.eu>
2018-01-06 14:51:13 +0100

    Cleaned Bind.hpp

--- devil/bind/Bind.hpp
+++ devil/bind/Bind.hpp
@@ -7,8 +7,8 @@
 #include "Traits.hpp"
 
 template <typename ReturnType>
-Caller<ReturnType, ReturnType(*)(void), 
-	typename Traits0::type> 
+Caller<ReturnType, ReturnType(*)(void),
+	typename Traits0::type>
 	bind(ReturnType (*func)(void))
 {
 	typedef TypeList0 ListType;
@@ -18,8 +18,8 @@ Caller<ReturnType, ReturnType(*)(void),
 }
 
 template <typename ReturnType, typename X1, typename Param1>
-Caller<ReturnType, ReturnType(*)(X1), 
-	typename Traits1<Param1>::type> 
+Caller<ReturnType, ReturnType(*)(X1),
+	typename Traits1<Param1>::type>
 	bind(ReturnType (*func)(X1), Param1 p1)
 {
 	typedef typename get_type<Param1>::type P1;
@@ -29,10 +29,10 @@ Caller<ReturnType, ReturnType(*)(X1),
 	return Caller<ReturnType, ReturnType(*)(X1), ListType>(func, list);
 }
 
-template <typename ReturnType, typename X1, typename X2, 
+template <typename ReturnType, typename X1, typename X2,
 	typename Param1, typename Param2>
-Caller<ReturnType, ReturnType(*)(X1, X2), 
-	typename Traits2<Param1, Param2>::type> 
+Caller<ReturnType, ReturnType(*)(X1, X2),
+	typename Traits2<Param1, Param2>::type>
 	bind(ReturnType (*func)(X1, X2), Param1 p1, Param2 p2)
 {
 	typedef typename get_type<Param1>::type P1;
@@ -40,7 +40,7 @@ Caller<ReturnType, ReturnType(*)(X1, X2),
 	typedef TypeList2<P1, P2> ListType;
 
 	ListType list(p1, p2);
-	return Caller<ReturnType, ReturnType(*)(X1, X2), ListType>(func, 
+	return Caller<ReturnType, ReturnType(*)(X1, X2), ListType>(func,
 									list);
 }
 
@@ -63,9 +63,9 @@ Caller<ReturnType, ReturnType(*)(X1, X2, X3),
 template <typename ReturnType, typename X1, typename X2, typename X3,
 	typename X4, typename Param1, typename Param2, typename Param3,
 	typename Param4>
-Caller<ReturnType, ReturnType(*)(X1, X2, X3, X4), 
-	typename Traits4<Param1, Param2, Param3, Param4>::type> 
-	bind(ReturnType (*func)(X1, X2, X3, X4), Param1 p1, Param2 p2, 
+Caller<ReturnType, ReturnType(*)(X1, X2, X3, X4),
+	typename Traits4<Param1, Param2, Param3, Param4>::type>
+	bind(ReturnType (*func)(X1, X2, X3, X4), Param1 p1, Param2 p2,
 		Param3 p3, Param4 p4)
 {
 	typedef typename get_type<Param1>::type P1;
@@ -82,7 +82,7 @@ Caller<ReturnType, ReturnType(*)(X1, X2, X3, X4),
 template <typename ReturnType, typename X1, typename X2, typename X3,
 	typename X4, typename X5, typename Param1, typename Param2,
 	typename Param3, typename Param4, typename Param5>
-Caller<ReturnType, ReturnType(*)(X1, X2, X3, X4, X5), 
+Caller<ReturnType, ReturnType(*)(X1, X2, X3, X4, X5),
 	typename Traits5<Param1, Param2, Param3, Param4, Param5>::type>
 	bind(ReturnType (*func)(X1, X2, X3, X4, X5), Param1 p1, Param2 p2,
 		Param3 p3, Param4 p4, Param5 p5)
@@ -103,8 +103,8 @@ template <typename ReturnType, typename X1, typename X2, typename X3,
 	typename X4, typename X5, typename X6, typename Param1,
 	typename Param2, typename Param3, typename Param4,
 	typename Param5, typename Param6>
-Caller<ReturnType, ReturnType(*)(X1, X2, X3, X4, X5, X6), 
-	typename Traits6<Param1, Param2, Param3, Param4, Param5, 
+Caller<ReturnType, ReturnType(*)(X1, X2, X3, X4, X5, X6),
+	typename Traits6<Param1, Param2, Param3, Param4, Param5,
 				Param6>::type>
 	bind(ReturnType (*func)(X1, X2, X3, X4, X5, X6), Param1 p1, Param2 p2,
 		Param3 p3, Param4 p4, Param5 p5, Param6 p6)
