d10b99f801c658ef4782533feb20686d57da81a7
Antoine Bache <antoine.bache@epitech.eu>
2018-01-06 01:31:59 +0100

    Changed enable_if to respect the subject

--- devil/EnableIf.hpp
+++ devil/EnableIf.hpp
@@ -1,15 +1,13 @@
 #ifndef ENABLE_IF_HPP_
 #define ENABLE_IF_HPP_
 
-template <bool C, typename T = void>
+template <bool C, typename T = void *>
 struct enable_if
-{
-};
-
-template <typename T>
-struct enable_if<true, T>
 {
 	typedef T value;
 };
+
+template <typename T>
+struct enable_if<false, T> {};
 
 #endif
